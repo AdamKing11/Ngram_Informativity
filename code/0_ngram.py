d9c6b769ee0a271780e6c2c7e3329207588b371b
AdamKing11 <adamking@email.arizona.edu>
2016-11-06 19:38:00 -0700

    further updates - moved a lot of the housekeeping functions to new file

--- code/0_ngram.py
+++ code/0_ngram.py
@@ -1,5 +1,9 @@
 import re, sys, math
 
+from 1_housekeeping import *
+
+# meat and potatoes
+
 def ngram_informativity(ngrams):
 	"""
 	takes in an ngram file and calculates average informativy
@@ -71,7 +75,13 @@ def build_ngrams(coca_file, ngram_size=2):
 		for line in rF:
 			c += 1
 			print("Reading line", c, end="\r")
-			words = clean_line(line).split(" ")
+			
+			######
+			# pre-processing takes out need to clean the line every time
+			######
+			#words = clean_line(line).split(" ")
+			words = line.rstrip().split(" ")
+
 			# we're going to loop through all words and build ngrams out of them
 			# because ngrams are "sliding windows", we start at the beginning and
 			# go to the last - (ngram_size-1) (i.e. in trigrams, we stop at end - 2)
@@ -89,74 +99,66 @@ def build_ngrams(coca_file, ngram_size=2):
 	print("\nDone reading.", c, "lines read in total.")
 	return ngram_d
 
-def good_ngram(n):
+def save_ngram_file(ngrams, outfile = None):
 	"""
-	takes an ngram and checks to make sure all words are good
-	"""
-	for w in n:
-		if not good_word(w):
-			return False
-	return True
-
-def good_word(w):
-	"""
-	takes a word and checks if we want to keep track of it
-	like, some junk in COCA is just noise
-	"""
-	if len(w) == 0:
-		return False
-
-	if re.search("^[:@.,\"]",w):
-		return False
-
-	# COCA has names of speakers in CAPS
-	if w == w.upper() and len(w) > 1:
-		return False
-
-	return True
-
-def clean_line(line):
-	"""
-	takes a line of text and returns a "cleaned" form of the line
-	
+	save the ngrams we generated
+	format will be: ngram TAB count
+		with all words in ngram separated by SPACE
+		(so we can tell them apart....)
 	"""
 
-	# the way COCA does it is is to split ""contractions"" into 
-	# separate words, we'll just plop them on the previous word
-	contractions = ("n't", "'re", "'s", "'me",
-		"'m")
 
-	l = line.rstrip().rsplit(" ")
-	line = ""
-	for w in l:
-		cw = clean_word(w)
-		# because COCA makes n't separate words
-		if cw in contractions:
-			line = line[:-1] + cw + " "
-		else:
-			line += cw + " "
+	# sort the ngrams
+	ngram_list = sorted(ngrams, key = lambda element: tuple(element[:-1]))
+	ngram_len = len(ngram_list[0])
 
-	return(line[:-1])
+	# give it a default name if we don't have one yet
+	if outfile == None:
+		outfile = str(ngram_len) + "-grams.txt"
 
-def clean_word(w):
+	with open(outfile, "w") as wF:
+		for n in ngram_list:
+			ngram_str = ""
+			for w in n:
+				ngram_str += w + " "
+			ngram_str = ngram_str[:-1]
+			wF.write(ngram_str + "\t" + str(ngrams[n]) + "\n")
+
+def save_informativity_file(infor, outfile = "avg_informativity.txt"):
 	"""
-	takes a word and "cleans" it, ie folds case and gets rid of weird stuff
+	format - word TAB informativity
 	"""
-	w = w.lower()
-	w = re.sub("(^[\-\.]|[\-\.]$)","",w)
-	return(w)
+
+	with open(outfile, "w") as wF:
+		for i in infor:
+			wF.write(i + "\t" + str(infor[i]) + "\n")
 
 
 if __name__ == "__main__":
+
+	# only do this the first time you run, afterwards,
+	# it should run much faster
+	should_preprocess = False
+
+	if should_preprocess:
+		preprocess_COCA()
+		print()
+		sys.exit(0)
+
 	# use the first argument as the COCA (or smaller version) to 
 	# calc everything
-	b = build_ngrams(sys.argv[1],4)
+	b = build_ngrams("COCA.clean.txt",2)
+
+	print("Done making n-grams. Saving....")
+	save_ngram_file(b)
 
 	print("\n10 most frequent ngrams:")
 	for n in sorted(b, key = lambda g: b[g], reverse=True)[:10]:
 		print(n,b[n])
 
 	i = ngram_informativity(b)
+	print("Done calculating informativity. Saving....")
+	save_informativity_file(i)
 
 	print("\n10 most informative words:")
 	for n in sorted(i, key = lambda g: i[g], reverse=True)[:10]:
